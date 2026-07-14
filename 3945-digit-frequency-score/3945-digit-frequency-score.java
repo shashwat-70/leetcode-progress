16c8256ef18b800c2a65ee412eca7fc3636b61c7
Shashwat Upadhyay <shashwatjnp7@gmail.com>
2026-07-15 00:14:00 +0530

    Time: 1 ms (99.15%), Space: 42.4 MB (91.79%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,14 +1,11 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        int[] freq=new int[10];
         int ans=0;
         while(n>0){
-            freq[n%10]++;
+            ans+=n%10;
             n/=10;
         }
-        for(int i=0;i<freq.length;i++){
-            ans+=i*freq[i];
-        }
+        
         return ans;
     }
 }
